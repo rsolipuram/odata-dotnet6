bd6378b928e0b753b4b507fdfba1b426cfdad0ca
Raghav <Raghavender.s@gmail.com>
2023-02-21 08:03:44 -0500

    intial commit

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Threading.Tasks;
+// using Microsoft.AspNetCore.Hosting;
+// using Microsoft.Extensions.Configuration;
+// using Microsoft.Extensions.Hosting;
+// using Microsoft.Extensions.Logging;
+
+// namespace quickstartcore
+// {
+//     public class Program
+//     {
+//         public static void Main(string[] args)
+//         {
+//             CreateHostBuilder(args).Build().Run();
+//         }
+
+//         public static IHostBuilder CreateHostBuilder(string[] args) =>
+//             Host.CreateDefaultBuilder(args)
+//                 .ConfigureWebHostDefaults(webBuilder =>
+//                 {
+//                     webBuilder.UseStartup<Startup>();
+//                 });
+//     }
+// }
+
+using Microsoft.AspNetCore.OData;
+using Microsoft.OpenApi.Models;
+using quickstartcore;
+using todo;
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddSingleton<IDocumentDBRepository<todo.Models.Item>>(o=>new DocumentDBRepository<todo.Models.Item>());
+builder.Services.AddControllers().AddOData(options => options.Select().Filter().OrderBy().AddRouteComponents(
+            routePrefix: "odata",
+            model: Startup.GetEdmModel()));
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "AspNet6WithOData", Version = "v1" });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Item}/{action=Index}/{id?}");
+
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AspNet2WithOData v1"));
+
+
+app.Run();
+
+
---
+++ Startup.cs
@@ -0,0 +1,78 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.OData;
+using Microsoft.OData.Edm;
+using Microsoft.OData.ModelBuilder;
+using todo.Models;
+using Microsoft.OpenApi.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OData.Edm;
+using Microsoft.OData.ModelBuilder;
+using Microsoft.OpenApi.Models;
+
+namespace quickstartcore
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+         public static IEdmModel GetEdmModel() 
+        { 
+            ODataConventionModelBuilder builder = new ODataConventionModelBuilder(); 
+            builder.EntitySet<Item>("Item"); 
+            return builder.GetEdmModel(); 
+        }
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            
+            services.AddControllersWithViews();
+            // services.AddControllers().AddOData(options => options.Select().Filter().OrderBy().AddRouteComponents(
+            // routePrefix: "odata",
+            // model: GetEdmModel()));
+
+            // services.AddSwaggerGen(c =>
+            // {
+            //     c.SwaggerDoc("v1", new OpenApiInfo { Title = "AspNet6WithOData", Version = "v1" });
+            // });
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseStaticFiles();
+
+            // app.UseSwagger();
+            // app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AspNet2WithOData v1"));
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
